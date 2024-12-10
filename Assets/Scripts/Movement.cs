049b25fc5fbc050f5baf971179d96c2067148612
Patrick Joseph Pronuevo <3xjoseph@gmail.com>
2024-12-10 14:30:10 +0800

    40 - 43. Rotation Input, Cinemachine, and Rigidbody Tuning
    - Added input actions for rocket's rotation.
    - Added cinemachine camera to follow the rocket.
    -- Used deadzone to keep the target in the intended frame.
    -- Used Hard Limits to keep it from going outside the frame.
    
    Rigidbody tuning for position and rotation constraints.
    - Added linear damping for drag.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,25 +1,58 @@
+using System;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
     [SerializeField] InputAction thrust;
-    [SerializeField] float thrustStrength = 100f;
+    [SerializeField] InputAction rotation;
+    [SerializeField] float thrustStrength = 1000f;
+    [SerializeField] float rotationStrength = 1000f;
 
     Rigidbody rb;
 
     void OnEnable() 
     {
         thrust.Enable();
+        rotation.Enable();
     }
 
     void Start() 
     {
         rb = GetComponent<Rigidbody>();
     }
-    void FixedUpdate() 
+    void FixedUpdate()
     {
-        if(thrust.IsPressed())
+        ProcessThrust();
+        ProcessRotation();
+
+    }
+
+    void ProcessRotation()
+    {
+        float rotationInput = rotation.ReadValue<float>();
+        if (rotationInput < 0)
+        {
+            ApplyRotation(rotationStrength);
+        }
+        else if (rotationInput > 0)
+        {
+            ApplyRotation(-rotationStrength);
+        }   
+           
+    }
+
+    void ApplyRotation(float rotationStrengthParameter)
+    {
+        rb.freezeRotation = true;
+        transform.Rotate(Vector3.forward * rotationStrengthParameter * Time.fixedDeltaTime);
+        rb.freezeRotation = false;
+    }
+
+    private void ProcessThrust()
+    {
+        if (thrust.IsPressed())
         {
             rb.AddRelativeForce((Vector3.up * thrustStrength) * Time.fixedDeltaTime);
         }
